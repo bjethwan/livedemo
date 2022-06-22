8fd9ce8729826d879fdb74a9798e9c6afa9d472d
bjethwan <jethwani.bipin@gmail.com>
2022-06-22 10:37:22 +0530

    new msg

--- src/main/java/com/temenos/livedemo/HelloController.java
+++ src/main/java/com/temenos/livedemo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Dear Temenosians, Greetings from Spring Boot!";
+        return "Dear Temenosians, Greetings from Spring Boot and Bipin Jethwani!";
     }
 }
